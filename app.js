25280d64747ce9599279d53f16347133317d0e77
JonasPuchinger <j.g.puchinger@kabelmail.net>
2019-06-20 17:08:09 +0200

    Trying to adjust app.js for cors requests

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
